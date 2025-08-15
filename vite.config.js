eac988c34534d41e29b6e07a239a1944f5c65043
Moazam Ali <amoazam589@gmail.com>
2025-08-15 05:26:11 -0700

    changes

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react()],
+    optimizeDeps: {
+        esbuildOptions: {
+            target: 'esnext' // allow top-level await in deps
+        },
+        include: ['three'] // ensure three.js gets processed
+    },
+    build: {
+        target: 'esnext' // allow top-level await in build output
+    }
 })
